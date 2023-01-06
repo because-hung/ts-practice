81860b64152870980c2ca90bd6c47436bb5ed19c
Becasuse-hung <4a33a006@stust.edu.tw>
2023-01-06 17:49:32 +0800

    add: 網路上的範例(Job List)練習

---
+++ src/components/JobsList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="job-list">
+    <p>Ordered by {{ order }}</p>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} NT dollars</p>
+        </div>
+        <div class="desc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat excepturi sed aut eum recusandae quaerat. Deserunt illum quod beatae quam earum, numquam perspiciatis amet ducimus incidunt illo ea officiis nemo omnis ad recusandae impedit soluta ullam unde. Porro illum ut culpa eius sed non accusantium soluta, error ullam, dicta veniam.
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import Job from '@/types/job'
+import OrderTerm from '@/types/OrderTerm'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  }
+})
+</script>
+
+<style scoped>
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobsList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { defineComponent, ref } from 'vue'
+import JobsList from '../components/JobsList.vue'
+import Job from '../types/job'
+import OrderTerm from '@/types/OrderTerm'
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { JobsList },
+  setup () {
+    // const state = reactive({
+    //   name: 'Barry',
+    //   age: 25 as string | number
+    // })
+    // const aaa = toRefs(state)
+    // console.log(aaa)
+    // // return { ...toRefs(state) }
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+    // return { name, age }
+
+    // 上面是練習語法
+
+    const jobs = ref<Job[]>([
+      { title: 'farmer', location: 'london', salary: 5000, id: '1' },
+      { title: 'police', location: 'USA', salary: 4100, id: '2' },
+      { title: 'teacher', location: 'brndon', salary: 3000, id: '3' },
+      { title: 'nurse', location: 'ddndon', salary: 3500, id: '4' },
+      { title: 'house', location: 'oaaon', salary: 5400, id: '5' }
+    ])
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   }
 })
 </script>
+<style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+</style>
